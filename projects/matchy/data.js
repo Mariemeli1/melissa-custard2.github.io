8707cbef0af3a06b398977d6244bfa8309a38441
Melissa Custard <custardmelissa1@gmail.com>
2022-10-03 21:41:40 +0000

    matchyy

--- projects/matchy/data.js
+++ projects/matchy/data.js
@@ -22,22 +22,35 @@ console.log(animal);
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+var noises = [];
 
+noises[0] = 'bock';
+noises.push('woof');
+noises.unshift('meow');
+noises[noises.length] = 'arrr';
+
+console.log(noises.length);
+console.log(noises.length-1);
+console.log(noises);
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+animal['noises'] = noises;
+animal.noises.push('eee');
 
+console.log(animal);
 
 
 /* *******************************************************************
  * Step 4 - Review
  *
  * 1. What are the different ways you can access properties on objects?
+ * Through dot and bracket notation and a for in loop
  *
  * 2. What are the different ways of accessing elements on arrays?
- *
+ * Through bracket notation and a for loop 
  * *******************************************************************
  */
 
@@ -53,13 +66,50 @@ console.log(animal);
 //////////////////////////////////////////////////////////////////////
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+//assigning my animals variable to an empty array
+var animals = [];
 
+//pushing animal array to my animals array
+animals.push(animal);
+console.log(animals);
+//created a duck object
+var duck = { 
+  species: 'duck', 
+  name: 'Jerome', 
+  noises: ['quack', 'honk', 'sneeze', 'woosh'] 
+};
+//pushing duck object into my animals array
+animals.push(duck);
+console.log(animals);
 
+//creating 2 more animal objects  with a species a name and 2 sounds
+
+var cat = {
+  species: 'cat',
+  name: 'Tink',
+  noises: ['rawr', 'mew']
+}
+//pushing cat to my animals array
+animals.push(cat);
+var pig = {
+  species: 'pig',
+  name: 'Chris P. Bacon',
+  noises: ['sizzle', 'crunch']
+}
+//pushing pig to my animals array
+animals.push(pig);
 
 //////////////////////////////////////////////////////////////////////
 // Step 7 - Making Friends ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+//assigning friends to an array because arrays hold lists
+
+var friends = [];
+
+function getRandom(anims){
+  return Math.random(friends.push(anims.name));
+}
 
 
 /**
