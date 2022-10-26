8e14c6cb19775d93879c50bd81c29b108b1280d3
Melissa Custard <custardmelissa1@gmail.com>
2022-10-26 01:48:47 +0000

    Eloquent Javascript Completed

--- projects/eloquent-javascript/chapter-05/exercises.js
+++ projects/eloquent-javascript/chapter-05/exercises.js
@@ -2,25 +2,82 @@
 // flatten /////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function flatten() {
-
+function flatten(arr) {
+  //using the reduce method to return a new array
+  let array = arr.reduce(function(acc, curr){
+    //concatenate the current element in arr into a empty array
+    return acc.concat((curr), []);
+  });//return the array
+  return array;
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // loop ////////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function loop() {
-
+function loop(value, test, update, body) {
+  //iterating through the test function on the current loop value and stops if that returns false
+  //and calls the update function to create a new value and starts from the beginning
+  for(let i = value; test(i); i = update(i)){
+    //calls the body function and giving it the current value.
+    body(i);
+  }
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // every ///////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function every() {
+function every(array, test) {
+    //determine if test was not passed in
+    if(test === undefined ){
+      //determine if array is an array
+      if(Array.isArray(array)){
+          //iterate through array
+          for(let i = 0; i < array.length; i++){
+              //determine if array[i] is truthy
+              if(!array[i]){
+                  //return false;
+                  return false;
+              }
+          }
+      } else { //else
+          //iterate through object
+          for(let key in array){
+              //determine if array[key] is truthy
+              if(!array[key]){
+                  //return false;
+                  return false;
+              }
+          }
+      }
 
+  } else{ //else
+      //determine if array is an array
+      if(Array.isArray(array)){
+          //iterate through array
+          for(let i = 0; i < array.length; i++){
+              //determine if invoking test on the params is not true
+              if(test(array[i], i, array) == false){
+                  //return false
+                  return false;
+              }
+          }
+      } else{ //else its an object
+          //iterate through object
+          for(let key in array){
+              //determine if invoking test on the params is not true
+              if(test(array[key], key, array) === false)
+              {   //return false;
+                  return false;
+              }
+          }
+
+      }
+  }//return true
+  return true;
 }
+
 
 // /////////////////////////////////////////////////////////////////////////////
 // dominantDirection ///////////////////////////////////////////////////////////
